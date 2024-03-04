abbe69b0acee54bd30ea7394f4981e8f95d62764
Ilias N <94550564+iliasThe@users.noreply.github.com>
2024-03-04 17:08:42 +0700

    Update jira_transition.py

--- jira_transition.py
+++ jira_transition.py
@@ -16,7 +16,7 @@ dirRep = "C:/Users/ilias/OneDrive/Рабочий стол/Мусор/"
 
 # соединение с Jira при помощи логин и пароля
 jira_options = {'server':'https://stp.gisoms.ffoms.gov.ru', 'verify':False}
-jira = JIRA(options=jira_options, basic_auth=('inuruev','04021999google'))
+jira = JIRA(options=jira_options, basic_auth=('логин','пароль'))
 
 # issue = jira.issue(f'FERZLSUP-{numbers}')
 # Список номеров задач для СУЗ
@@ -81,7 +81,7 @@ jql_my_tasks = 'project = FERZLSUP AND status in (Закрыта, Решено) 
 
 def collect_task(jql, amount: int):
     cnt_task = 0
-    print(bot.send_message(chat_id='-1001660599100', text=f'Начало скрипта, кол-во: {amount} -' + f' {datetime.now()}'))
+    print(bot.send_message(chat_id='', text=f'Начало скрипта, кол-во: {amount} -' + f' {datetime.now()}'))
     while cnt_task <=amount:
         jira_key = jira.search_issues(jql, maxResults = 200)
         for keyid in range(len(jira_key)):
@@ -90,11 +90,11 @@ def collect_task(jql, amount: int):
             # задача в Jira
             issue = jira.issue(key_name)
             # апдейт исполнителя
-            issue.update(assignee={'name': 'inuruev'})
+            issue.update(assignee={'name': 'логин'})
             # вывод
             print(issue.fields.assignee, key_name)
             cnt_task = cnt_task + 1
             time.sleep(3)
-    print(bot.send_message(chat_id='-1001660599100', text = 'Конец скрипта ' + f'{datetime.now()}'))
+    print(bot.send_message(chat_id='', text = 'Конец скрипта ' + f'{datetime.now()}'))
 
 collect_task(jql_epotehin, 3)
